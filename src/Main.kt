63cbd57788cf8fdbccff03461d13431ae22dc28d
Theo Fortune <theofortunefunction@gmail.com>
2024-05-24 03:03:07 +0100

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+	val name = "Fortune"
+	println(name)
+	var a = 10
+	var b = 3
+	println(a + b)
+	println(a - b)
+	println(a * b)
+	println(a / b)
+	println(a % b)
+
+	println(a > b)
+	println(a < b)
+
+	var increment = a++
+	var decrement = b--
+	println(increment)
+	println(decrement)
+}
